c4cc4ebac0bd7cc08508f8e9585740d6806215e3
ChristianD <67894174+ChristianDae@users.noreply.github.com>
2020-07-28 15:57:48 +0200

    ewercice 1.1 à 1.3

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,17 @@
+/* Exercice 1.1 */
+/*
+let hello = 'hello, worlds';
+console.log(hello);
+*/
+/* Exercice 1.2 */
+/*
+let message = "Bonjour je m'apelle";
+let prenom = 'Christian';
+console.log(message + ' ' + prenom);
+*/
+/* Exercice 1.3 */
+
+let iam = "j'ai";
+let age = 36;
+let ans = 'ans';
+console.log(iam + ' ' + age + ' ' + ans);
